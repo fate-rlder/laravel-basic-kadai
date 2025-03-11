4d90a8c0800ec18c412a9a793e335834483870ba
fate-rlder <fate.rlder4620@gmail.com>
2025-03-11 12:23:37 +0900

    no message

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,15 +1,31 @@
-<!DOCTYPE html>
+<<!DOCTYPE html>
 <html lang="ja">
 
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Laravel基礎</title>
+   <title>Laravel課題</title>
 </head>
 
 <body>
-   <p>投稿一覧</p>
-  
+   <table>
+       <tr>
+           <th>ID</th>
+           <th>タイトル</th>
+           <th>本文</th>
+           <th>作成日時</th>
+           <th>更新日時</th>
+       </tr>
+       @foreach($posts as $posts)
+           <tr>
+               <td>{{ $posts->id }}</td>
+               <td>{{ $posts->title }}</td>
+               <td>{{ $posts->content }}</td>
+               <td>{{ $posts->created_at }}</td>
+               <td>{{ $posts->updated_at }}</td>
+           </tr>            
+       @endforeach       
+   </table>
 </body>
 
 </html>
